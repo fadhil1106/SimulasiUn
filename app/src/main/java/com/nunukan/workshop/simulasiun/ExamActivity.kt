777bb9b99dc968006b192827ed64e088c9464829
Fuad Fadhil Azzar <fuadf.512@gmail.com>
2018-11-19 22:46:28 +0800

    Finish Exam Layout

---
+++ app/src/main/java/com/nunukan/workshop/simulasiun/ExamActivity.kt
@@ -0,0 +1,12 @@
+package com.nunukan.workshop.simulasiun
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exam)
+    }
+}
