c0bdcb7a3a107d3c76d096924bcb49748e3ffa2c
Fuad Fadhil Azzar <fuadf.512@gmail.com>
2018-11-20 13:24:16 +0800

    Finish Exam Layout

---
+++ app/src/main/java/com/nunukan/workshop/simulasiun/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.nunukan.workshop.simulasiun
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
